00e346051c7cded17bee0e9c5243f508fd02867b
Henri Tremblay <henri.tremblay@gmail.com>
2010-05-22 13:45:48 +0000

    Move to sonatyoe repository

--- refresh-website.sh
+++ refresh-website.sh
@@ -8,6 +8,5 @@ cd /home/groups/e/ea/easymock \
 && rm -rf htdocs2 \
 && svn export http://easymock.svn.sourceforge.net/svnroot/easymock/trunk/website htdocs2  \
 && chmod -R g+w htdocs2 \
-&& mv htdocs/maven htdocs2/maven \
 && rm -rf htdocs \
 && mv htdocs2 htdocs
