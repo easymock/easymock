9a08496c66f306366ab9a4dccd9b79f40a5189fc
Henri Tremblay <henri.tremblay@gmail.com>
2013-03-27 00:32:52 +0000

    Add missing licenses

--- easymock/src/test/java/org/easymock/tests/MockTypeTest.java
+++ easymock/src/test/java/org/easymock/tests/MockTypeTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2001-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.easymock.tests;
 
 import static org.junit.Assert.*;
@@ -9,6 +24,11 @@ import org.easymock.MockType;
 import org.easymock.internal.MocksControl;
 import org.junit.Test;
 
+/**
+ * Test mocks create with a MockType
+ *  
+ * @author Henri Tremblay
+ */
 public class MockTypeTest {
 
     private final EasyMockSupport support = new EasyMockSupport();
