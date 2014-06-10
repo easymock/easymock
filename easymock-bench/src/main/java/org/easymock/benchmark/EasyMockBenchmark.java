e5ff05051aaf318222f6406d927bf249e92d027f
Henri Tremblay <henri.tremblay@gmail.com>
2014-06-10 22:51:04 +0200

    Upgrade to JMH 0.9

--- easymock-bench/src/main/java/org/easymock/benchmark/EasyMockBenchmark.java
+++ easymock-bench/src/main/java/org/easymock/benchmark/EasyMockBenchmark.java
@@ -1,11 +1,19 @@
 package org.easymock.benchmark;
 
-import org.easymock.EasyMock;
-import org.openjdk.jmh.annotations.*;
-
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import org.easymock.EasyMock;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 @Fork(2)
 @Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
@@ -13,18 +21,18 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
-public class Benchmark {
+public class EasyMockBenchmark {
 
     private Class<?> anInterface = List.class;
 
     private Class<?> aClass = ArrayList.class;
 
-    @GenerateMicroBenchmark
+    @Benchmark
     public Object list() {
         return EasyMock.createMock(anInterface);
     }
 
-    @GenerateMicroBenchmark
+    @Benchmark
     public Object arrayList() {
         return EasyMock.createMock(aClass);
     }
