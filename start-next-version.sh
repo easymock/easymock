9c1ccb81178fe168f20c02b5078e8d9ad65f51f1
Henri Tremblay <henri@tremblay.pro>
2014-10-20 07:58:26 -0400

    Comments on the parameters needed

--- start-next-version.sh
+++ start-next-version.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script expects:
+# - jira_user / jira_password to be available environment variables
+# - the next version to be received as the first parameter
+
 if [ "$1" == "" ]
     echo "Next version should be provided"
     exit 1
@@ -18,4 +22,4 @@ git commit -am "Start version $version"
 git push
 
 # Create next version in Jira (http://jira.codehaus.org/plugins/servlet/project-config/EASYMOCK/versions)
-curl -D- -u $user:$password -X POST --data '{ "description": "EasyMock $version", "name": "$version", "archived": false, "released": false, "project": "EASYMOCK", "projectId": 12103 }' -H "Content-Type:application/json" https://jira.codehaus.org/rest/api/2/version
+curl -D- -u ${jira_user}:${jira_password} -X POST --data '{ "description": "EasyMock $version", "name": "$version", "archived": false, "released": false, "project": "EASYMOCK", "projectId": 12103 }' -H "Content-Type:application/json" https://jira.codehaus.org/rest/api/2/version
