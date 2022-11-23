57cab17bde2c171306b27dbe385869901d7ebc10
Henri Tremblay <henri.tremblay@gmail.com>
2022-11-23 08:58:34 -0500

    Unused import

--- core/src/main/java/org/easymock/internal/ClassProxyFactory.java
+++ core/src/main/java/org/easymock/internal/ClassProxyFactory.java
@@ -36,7 +36,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
