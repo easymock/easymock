3e0ebf515541c71909feda4ef4011dc912fe0a9a
Henri Tremblay <henri.tremblay@gmail.com>
2022-11-28 08:52:28 -0500

    Attempt to use TypeCache. ClassProxyFactoryTest is failing

--- core/src/test/java/org/easymock/internal/ClassProxyFactoryTest.java
+++ core/src/test/java/org/easymock/internal/ClassProxyFactoryTest.java
@@ -17,18 +17,27 @@ package org.easymock.internal;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.implementation.InvocationHandlerAdapter;
-import org.junit.Test;
+import org.easymock.ConstructorArgs;
+import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static net.bytebuddy.matcher.ElementMatchers.any;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.mock;
+import static org.easymock.EasyMock.replay;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class ClassProxyFactoryTest {
 
     @Test
-    public void testInterception() throws Exception {
+    void testInterception() throws Exception {
         AtomicBoolean called = new AtomicBoolean(false);
 
         Class<?> clazz = new ByteBuddy()
@@ -48,5 +57,28 @@ public class ClassProxyFactoryTest {
 
     public void hello() {
         fail("Should not be called since it's proxied");
+    }
+
+    @Test
+    void testSameClassInstance() throws Exception {
+        ClassProxyFactory factory = new ClassProxyFactory();
+        Constructor<?> constructor = getClass().getConstructor();
+        ClassProxyFactoryTest t1 = factory.createProxy(getClass(), (proxy, method, args) -> null, null, new ConstructorArgs(constructor));
+        ClassProxyFactoryTest t2 = factory.createProxy(getClass(), (proxy, method, args) -> null, null, new ConstructorArgs(constructor));
+        assertSame(t1.getClass(), t2.getClass());
+        assertNotSame(t1, t2);
+    }
+
+    @Test
+    void twoMocksOfTheSameType() {
+        ArrayList<?> list1 = mock(ArrayList.class);
+        expect(list1.size()).andReturn(1);
+        replay(list1);
+        assertEquals(1, list1.size());
+
+        ArrayList<?> list2 = mock(ArrayList.class);
+        expect(list2.size()).andReturn(2);
+        replay(list2);
+        assertEquals(2, list2.size());
     }
 }
